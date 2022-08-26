94eb96e51a32221ef62af9e5d37fd3ca5770a3cb
Homework3 <laneevama@gmail.com>
2022-08-26 15:49:49 +0300

    Выполнила дз, но не полностью.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Author bookAuthor1 = new Author("Лев","Толстой");
+        Author bookAuthor2 = new Author("Михаил","Шолохов");
+
+        Book book1 = new Book("Война и мир", bookAuthor1,1864);
+        Book book2 = new Book("Тихий Дон", bookAuthor2,1925);
+
+        book1.setyearPublication(1863);
+
+
+        System.out.println("book 1: " + book1.getbookTitle() + ", "+book1.getbookAuthor() +", "+ book1.getyearPublication() );
+        System.out.println("book 2: " + book2.getbookTitle() + ", "+book2.getbookAuthor() +", "+ book2.getyearPublication() );
+
+
     }
 }
